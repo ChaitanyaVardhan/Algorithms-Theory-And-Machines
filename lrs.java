c136567fb0d3ecdba3ef2cf8b496a184bee5ecd6
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-07-09 11:42:34 +0000

    longest repeated subsequence by brute force

--- lrs.java
+++ lrs.java
@@ -9,4 +9,21 @@ public class lrs {
 	return s.substring(0, N);	
     }
 
+    public static String lrs(String s) {
+	int N = s.length();
+	String lrs = "";
+
+	for (int i =0; i < N; i++) {
+	    for (int j = i + 1; j < N; j++) {
+		String x = lcp(s.substring(i, N), s.substring(j, N));
+		if (x.length() > lrs.length()) lrs = x;
+	    }
+	}
+	return lrs;
+    }
+
+    public static void main(String[] args) {
+	String s = args[0];
+	StdOut.println(lrs(s));
+    }
 }
