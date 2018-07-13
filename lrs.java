3aac50ca9573750cc109b4a90ece22d508c908f4
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-07-13 09:28:43 +0000

    longest repeated substring through brute force and suffix string methods

--- lrs.java
+++ lrs.java
@@ -1,7 +1,10 @@
+/* Longest repeated substring in a string                   */
+/* Execution: javac lrs <string>                            */
+
 public class lrs {
     public static String lcp(String s, String t) {
 	int N = Math.min(s.length(), t.length());
-	
+
 	for (int i = 0; i < N; i++) {
 	    if (s.charAt(i) != t.charAt(i))
 		return s.substring(0, i);
@@ -25,18 +28,26 @@ public class lrs {
     public static String lrsSuffixString(String s) {
 	int N = s.length();
 	String[] suffixes = new String[N];
+	String lrs = "";
 	for (int i = 0; i < N; i++) 
 	    suffixes[i] = s.substring(i, N);
+
+	Merge.sort(suffixes);
 
 	for (int i = 0; i < N; i++)
 	    StdOut.println(suffixes[i]);
 
-	return s;
+	for (int i = 0; i < N - 1; i++) {
+	    String x  = lcp(suffixes[i], suffixes[i+1]);
+	    if (x.length() > lrs.length()) lrs = x;
+	}
+	
+	return lrs;
     }
 
     public static void main(String[] args) {
 	String s = args[0];
-	StdOut.println(lrs(s));
-	lrsSuffixString(s);
+	StdOut.println("longest substring thorugh brute force :" + lrs(s));
+	StdOut.println("longest substring through suffix string method : " + lrsSuffixString(s));
     }
 }
