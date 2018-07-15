a7dcaf51b39f5487eb8d9b8e0c6e54858acdb19f
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-07-15 11:27:09 +0000

    Sort Strings received from StdIn using a BST

---
+++ Sort.java
@@ -0,0 +1,14 @@
+/***********************************************************/
+/* Sort the strings from StdIn using BST                   */
+/* Execution: java Sort < input.txt                        */
+
+public class Sort {
+    public static void main(String[] args){
+	BST<String, Integer> st = new BST<String, Integer>();
+	while (!StdIn.hasNextLine())
+	    st.put(StdIn.readLine(), 0);
+
+	for (String s : st.keys())
+	    StdOut.println(s);
+    }
+}
